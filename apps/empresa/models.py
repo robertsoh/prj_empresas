2ac5a9ed45fb8c97afd73427fb5ff575ffa67a53
Robertson <robertson.ore@gmail.com>
2018-05-25 10:29:52 -0500

    Agrega modelo empresa

--- apps/empresa/models.py
+++ apps/empresa/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class TipoContribuyente(models.Model):
+    codigo = models.CharField('RUC', max_length=6)
+    nombre = models.CharField('Nombre', max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Empresa(models.Model):
+    ESTADO_ACTIVO = 'ACTIVO'
+    ESTADO_INACTIVO = 'INACTIVO'
+    ESTADO_CHOICES = (
+        (ESTADO_ACTIVO, 'Activo'),
+        (ESTADO_INACTIVO, 'Inactivo'),
+    )
+    CONDICION_HABIDO = 'HABIDO'
+    CONDICION_NOHABIDO = 'NOHABIDO'
+    CONDICION_CHOICES = (
+        (CONDICION_HABIDO, 'Habido'),
+        (CONDICION_NOHABIDO, 'No habido')
+    )
+
+    ruc = models.CharField('RUC', max_length=15)
+    nombre = models.CharField('Nombre', max_length=150)
+    tipo_contribuyente = models.ForeignKey(TipoContribuyente, verbose_name='Tipo contribuyente')
+    nombre_comercial = models.CharField('Nombre comercial', max_length=150, blank=True, null=True)
+    fecha_inscripcion = models.DateField('Fecha inscripción', blank=True, null=True)
+    estado = models.CharField('Estado', max_length=10, choices=ESTADO_CHOICES, default=ESTADO_INACTIVO)
+    condicion = models.CharField('Condición', max_length=10, choices=CONDICION_CHOICES, default= CONDICION_HABIDO)
+    domicilio_fiscal = models.CharField('Domicilio fiscal', max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.ruc, self.nombre)
--- config/settings/base.py
+++ config/settings/base.py
@@ -27,8 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
-
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'apps.common',
+    'apps.empresa',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +124,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'apps', 'static'),
